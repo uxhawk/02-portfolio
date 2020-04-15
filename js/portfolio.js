f6d72287052a1d57b6168f1dd6f76a32847db1fa
David Hawkins <uxhawk@gmail.com>
2020-04-15 10:08:37 -0400

    replaced local storage function to be more concise; used jQuery for theme btn click

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,50 +1,51 @@
-var lightTheme;
+var theme = "light";
 const htmlTag = document.getElementsByTagName("html")[0];
 const body = document.getElementsByName("body")[0];
 const themeBtn = document.getElementById("theme-btn");
 
-// function toggleTheme() {
-//     if (htmlTag.hasAttribute("data-theme")) {
-//         body.style.transition = "500ms ease-in-out";
-//         lightTheme = true;
-//         storageVal = JSON.stringify(lightTheme);
-//         window.sessionStorage.setItem("theme", storageVal);
-//         htmlTag.removeAttribute("data-theme");
-//         body.style.removeProperty = "transition";
-//         return;
-//     }
-//     body.style.transition = "500ms ease-in-out";
-//     htmlTag.setAttribute("data-theme", "dark");
-//     lightTheme = false;
-//     storageVal = JSON.stringify(lightTheme);
-//     window.sessionStorage.setItem("theme", storageVal);
-//     body.style.removeProperty = "transition";
-// }
+function init() {
+    var storedTheme = localStorage.getItem("theme");
 
-function checkTheme() {
-    if (window.sessionStorage.getItem("theme") === "false") {
-        htmlTag.setAttribute("data-theme", "dark");
+    if (storedTheme !== null) {
+        theme = storedTheme;
     }
+    setStyle();
+
 }
 
-themeBtn.addEventListener("click", function() {
+function storeTheme() {
+    localStorage.setItem("theme", theme);
+}
 
-    if (htmlTag.hasAttribute("data-theme")) {
-
-        lightTheme = true;
-        storageVal = JSON.stringify(lightTheme);
-        window.sessionStorage.setItem("theme", storageVal);
+function setStyle() {
+    if (theme === "light") {
         htmlTag.removeAttribute("data-theme");
         themeBtn.classList.add("far", "fa-moon");
         themeBtn.classList.remove("fas", "fa-sun");
-        return;
+    } else {
+        htmlTag.setAttribute("data-theme", "dark");
+        themeBtn.classList.remove("far", "fa-moon");
+        themeBtn.classList.add("fas", "fa-sun");
     }
-    // body.style.transition = "500ms ease-in-out";
-    htmlTag.setAttribute("data-theme", "dark");
-    lightTheme = false;
-    storageVal = JSON.stringify(lightTheme);
-    window.sessionStorage.setItem("theme", storageVal);
-    themeBtn.classList.remove("far", "fa-moon");
-    themeBtn.classList.add("fas", "fa-sun");
+}
 
-})
+function toggleTheme() {
+    if (theme === "light") {
+        theme = "dark";
+        storeTheme();
+
+        htmlTag.setAttribute("data-theme", "dark");
+        themeBtn.classList.remove("far", "fa-moon");
+        themeBtn.classList.add("fas", "fa-sun");
+    } else {
+        theme = "light"
+        storeTheme();
+
+        htmlTag.removeAttribute("data-theme");
+        themeBtn.classList.add("far", "fa-moon");
+        themeBtn.classList.remove("fas", "fa-sun");
+    }
+}
+
+init();
+$("#theme-btn").click(toggleTheme);
